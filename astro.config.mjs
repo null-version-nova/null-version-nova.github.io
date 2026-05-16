f49201d91b1b98d8457c25234a527f9c3f2522d9
null-version-nova <ethcoyle@gmail.com>
2026-05-15 21:09:44 -0500

    github.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://null-version-nova.github.io",
+});
