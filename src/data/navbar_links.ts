f02fa3dbb2c65f1857c4bf0302e8e0e84848f211
null-version-nova <ethcoyle@gmail.com>
2026-05-23 17:23:08 -0500

    buttons. and music

--- src/data/navbar_links.ts
+++ src/data/navbar_links.ts
@@ -2,7 +2,8 @@ import type Link from "../types/link";
 
 const NAVBAR_LINKS: Link[] = [
     { label: "home", href: "/" },
-    { label: "blog", href: "/blog" }
+    { label: "blog", href: "/blog" },
+    { label: "music", href: "/music" }
 ]
 
 export default NAVBAR_LINKS;
