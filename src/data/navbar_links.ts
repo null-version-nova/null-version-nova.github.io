a000d6668f0c37ec29058e265b570966f899bed0
null-version-nova <ethcoyle@gmail.com>
2026-05-27 02:56:09 -0500

    adding about to navigation

--- src/data/navbar_links.ts
+++ src/data/navbar_links.ts
@@ -2,8 +2,9 @@ import type Link from "../types/link";
 
 const NAVBAR_LINKS: Link[] = [
     { label: "home", href: "/" },
+    { label: "about", href: "/about" },
     { label: "blog", href: "/blog" },
-    { label: "music", href: "/music" }
+    { label: "music", href: "/music" },
 ]
 
 export default NAVBAR_LINKS;
